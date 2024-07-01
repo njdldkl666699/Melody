19a8e72a1139afb45309d0a8373d2ecba79ea290
noslanker <juice1750@outlook.com>
2024-07-01 14:46:01 +0800

    测试

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "menuwidget.h"
 
 #include <QApplication>
+#include <QString>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     menuWidget w;
+    QString str = "hello.\n";
+    qDebug() << str;
+
+
+
     w.show();
     return a.exec();//end
 }
