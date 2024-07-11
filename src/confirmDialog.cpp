931a4ec7f668870610148bbbb95f776ee822cfbd
Le Zhang <zhangle666699@qq.com>
2024-07-11 21:19:58 +0800

    1、修改一个错误的铺面文件； 2、设计了退出界面

--- src/confirmDialog.cpp
+++ src/confirmDialog.cpp
@@ -7,14 +7,14 @@ ConfirmDialog::ConfirmDialog(QWidget *parent)
 	
 	setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
 	this->setModal(true);
+	QPixmap backgroundPNG("./res/background/exitDialog.png");
+	backgroundPNG = backgroundPNG.scaled(this->size(),
+		Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+	ui.background->setPixmap(backgroundPNG);
 	// set half-transparent :reserve
 	//setWindowOpacity(0.5);
 	//setAttribute(Qt::WA_TranslucentBackground);
 	//setStyleSheet("background:rgba(255, 255, 255,0);"); // 透明度为200，0完全透明，255完全不透明
-
-	ui.label_background->raise();
-	ui.pushButton_backMenu->raise();
-	ui.pushButton_exit->raise();
 
 	this->connect(ui.pushButton_exit, &QPushButton::clicked, this, &ConfirmDialog::on_pushButton_exit_clicked);
 	this->connect(ui.pushButton_backMenu, &QPushButton::clicked, this, &ConfirmDialog::on_pushButton_backMenu_clicked);
